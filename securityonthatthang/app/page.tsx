1101e2596c4da9641616fec7b5ce0532d1edc140
StargazerCatsriel <danobrovold@gmail.com>
2026-02-06 13:25:15 +0100

    Add Validation to make sure the user actually wrote stuff

--- securityonthatthang/app/page.tsx
+++ securityonthatthang/app/page.tsx
@@ -1,37 +1,41 @@
 "use client"
 import Box from "@/components/formbox";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Navbar from "@/components/ui/topbar";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useActionState, useEffect, useState } from "react";
 
 export default function Home() {
   async function WriteWord() {
+    if(name !== "" && email !== "" && adress !== ""){
+      setHasFailed(false)
       const res = await fetch("/api/addToJSON", {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ 
-                name: name,
-                email: email,
-                address: adress
-              })
-        });
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ 
+          name: name,
+          email: email,
+          address: adress
+        })
+      });
 
-        if(res.status === 400){
-          console.log("hi");
-        }
-        console.log(res);
+      if(res.status === 400){
+        console.log("hi");
+      }
+      console.log(res);
+    } else {
+      setHasFailed(true)
     }
-
+  }
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [adress, setAdress] = useState("");
+    const [hasFailed, setHasFailed] = useState(false)
 
   return (
-    <div className="bg-[url(/ironlungbg2.png)] bg-cover bg-stretch h-fit sm:h-[100vh]">
+    <div className="bg-[url(/ironlungbg2.png)] bg-cover bg-stretch min-h-screen h-full1">
       <Navbar></Navbar>
 
       <div className="flex justify-center mt-20 p-5">
@@ -46,6 +50,12 @@ export default function Home() {
 
       <div className="text-white font-bold flex flex-col justify-center items-center">
         <h1 className="sm:text-3xl mt-10">Registrer deg som blodgiver, redd liv</h1>
+
+        {
+          hasFailed && <div className="bg-red-200 border border-red-600 p-5 rounded-sm text-red-600 mt-10">
+            <h1>Du mangler å fylle inn i en eller flere av feltene. Prøv igjen.</h1>
+          </div>
+        }
 
         <form className="mt-10" onSubmit={(e) => {
           e.preventDefault();
